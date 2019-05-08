684d24d719b785725e736671faf2681232ecc394
mala0858 <malay.biswal@rackspace.com>
2019-05-08 18:41:19 -0500

    min Stack added comment

--- leetcode/minStack.py
+++ leetcode/minStack.py
@@ -1,22 +1,23 @@
 #https://leetcode.com/problems/min-stack/
+# Need 2 stacks. minStack to track minimum elment so minimum element can be searched by O(1)
 class MinStack(object):
     def __init__(self):
         self.stack=[]
         self.minStack=[]
     def push(self,x):
         self.stack.append(x)
-        if(len(self.minStack)==0):
+        if(len(self.minStack)==0):#1st element append to miStack too
             self.minStack.append(x)
         else:
             topMinElement=self.minStack[-1]
-            if x<=topMinElement:
+            if x<=topMinElement: #If elementgettuing pushed is less than minstack top element then push to MinStack
                 self.minStack.append(x)
             
 
         print("APPENDING:",self.stack,self.minStack)
 
     def pop(self):
-        if(self.stack[-1]==self.minStack[-1]):
+        if(self.stack[-1]==self.minStack[-1]): # If both stacks top elemnts matching pop from minStack as well
             self.minStack.pop()
             self.stack.pop()
         else:
